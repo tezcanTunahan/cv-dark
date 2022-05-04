7585ae4053c0a8ee0cccdd46f30d681da9039485
Tunahan Tezcan <ttezcan.1999@gmail.com>
2022-05-05 01:56:28 +0300

    proje fnshed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,19 +5,27 @@ import {
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons';
 
-export default function Navbar() {
+export default function Navbar({ hide, setHide }) {
   return (
-    <div className='navbar'>
+    <div className={`navbar ${hide && 'navbar--hide'}`}>
       <div className='navbar__top'>
         <h3>Tunahan</h3>
         <p>Web developer</p>
       </div>
       <div className='navbar__mid'>
         <nav className='navbar__mid__nav'>
-          <a href='#about'>About</a>
-          <a href='#work'>Work</a>
-          <a href='#mySkills'>My skills</a>
-          <a href='#contact'>Contact</a>
+          <a onClick={() => setHide(true)} href='#about'>
+            About
+          </a>
+          <a onClick={() => setHide(true)} href='#work'>
+            Work
+          </a>
+          <a onClick={() => setHide(true)} href='#mySkills'>
+            My skills
+          </a>
+          <a onClick={() => setHide(true)} href='#contact'>
+            Contact
+          </a>
           <a>Blog</a>
         </nav>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import Container from '../components/Container';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import Welcome from '../components/Welcome';
 import Work from '../components/Work';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
+import { useState } from 'react';
 export default function Home() {
+  const [hide, setHide] = useState(false);
+
+  function clikHandle() {
+    setHide(!hide);
+  }
+
   return (
     <Container>
       <div className='home'>
         <div className='home__navbar'>
-          <Navbar />
+          <div onClick={clikHandle} className='home__navbar__button'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <Navbar hide={hide} setHide={setHide} />
         </div>
         <div className='home__page'>
           <main>
